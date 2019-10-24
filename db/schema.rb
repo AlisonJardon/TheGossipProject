5fd4993bf93ab73996d0a5f53ba33bdd91ed8243
AlisonJardon <alison.jardon@gmail.com>
2019-10-25 00:00:59 +0200

    next commit

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_10_24_204415) do
   end
 
   create_table "tags", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
